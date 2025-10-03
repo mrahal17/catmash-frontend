551c4f8128f348399cac237be51b8ad71026ed76
mrahal17 <maya.rahal17@gmail.com>
2025-10-03 23:28:59 +0000

    Create CatService with getAll method only

---
+++ src/app/service/cat.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Cat } from '../model/cat.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class CatService {
+  private apiUrl = '/api/cats/';
+
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<Cat[]> {
+    return this.http.get<Cat[]>(this.apiUrl);
+  }
+}

4fc9f85a3a3d5b6201c23e3abc42ebe943f38e58
mrahal17 <maya.rahal17@gmail.com>
2025-10-05 16:12:12 +0000

    Fix routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { GeneralRankingComponent } from './general-ranking/general-ranking.component';
+import { VotingBattleComponent } from './voting-battle/voting-battle.component';
 
 export const routes: Routes = [
-  { path: '', component: GeneralRankingComponent },     
+  { path: '', component: GeneralRankingComponent, pathMatch: 'full' },
   { path: 'general-ranking', component: GeneralRankingComponent },
-  { path: '**', redirectTo: '' }               
+  { path: 'voting-battle', component: VotingBattleComponent },
+  { path: '**', redirectTo: '' }
 ];
